c329ce123f913510911aee26001f4ad951807cc1
TheArqsz <arek.maruszczak@gmail.com>
2021-02-28 17:51:40 +0000

    Update main script

--- deep-freezer.sh
+++ deep-freezer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-# trap cleanup SIGINT SIGTERM ERR EXIT
+trap cleanup SIGINT SIGTERM ERR EXIT
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
